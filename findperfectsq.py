333a6751afd1b1507c823401328ef4953e6b907c
affan.zaidi <affan.zaidi@vithamastech.com>
2022-12-05 19:54:57 +0530

    new Problem

--- findperfectsq.py
+++ findperfectsq.py
@@ -1,1 +1,13 @@
-print("Hello world")
+import math
+
+num =  int(input("Enter a Number:"))
+
+
+def isperfectsquare(n):
+    root = math.sqrt(n)
+    if int (root + 0.5) ** 2 == n:
+        print(f"{n} is a perfect square")
+    else:
+        print(f"{n} is not a perfect square")
+
+isperfectsquare(num)
